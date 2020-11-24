c0611e5496c2bcf608c3a5012469049031c23c90
aeeu <alexandre.esquerdo@gft.com>
2020-11-24 14:02:54 +0100

    added final step for authorisation jwt

--- src/Action/CartAddAction.php
+++ src/Action/CartAddAction.php
@@ -6,6 +6,11 @@ use App\Domain\User\Service\CartService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use RedBeanPHP\R;
+include_once '/Applications/XAMPP/xamppfiles/htdocs/API/lib/php-jwt-master/src/BeforeValidException.php';
+include_once '/Applications/XAMPP/xamppfiles/htdocs/API/lib/php-jwt-master/src/ExpiredException.php';
+include_once '/Applications/XAMPP/xamppfiles/htdocs/API/lib/php-jwt-master/src/SignatureInvalidException.php';
+include_once '/Applications/XAMPP/xamppfiles/htdocs/API/lib/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
 
 /**
  * Action
@@ -38,29 +43,41 @@ final class CartAddAction
      */
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args = []): ResponseInterface
     {
-        // First we create a Bean or row, empty
-        $newCart = R::dispense('carts');
-        $newProductCart = R::dispense('product_cart');
+        $key = "JWTToken";
         $parsedBody = $request->getParsedBody();
-        //$jwt = $request->getHeader('Authorization');
-        //verify_idtoken
-        //$verify = JWT::verify($jwt, "JWTToken", array('HS256'));
+        $products = $parsedBody['products'];
+        $jwt = $request->getHeader('Authorization');
+        $jwt = str_replace('Bearer ', '', $jwt[0]);
+        $decoded = JWT::decode($jwt, $key, ['HS256']);
 
-        // Now we fill the new row with the params
-        $newCart->user_id = $parsedBody['userId'];
-        $newCart->date = $parsedBody['date'];
-        $newCart->amount = $parsedBody['amount'];
-        // Invoke the Domain with inputs and retain the result
-        $addCardList = R::store($newCart);
+        if ($decoded) {
+            // First we create a Bean or row, empty
+            $newCart = R::dispense('carts');
+            $beans = array();
+            // Now we fill the new row with the params
+            $newCart->user_id = $parsedBody['userId'];
+            $newCart->date = $parsedBody['date'];
+            $newCart->amount = $parsedBody['amount'];
 
-        $newProductCart->product_id = $parsedBody['productId'];
-        $newProductCart->quantity = $parsedBody['quantity'];
-        $newProductCart->cartId = $addCardList;
-        $addProductCart = R::store($newProductCart);
-
-        // Build the HTTP response
-        $response->getBody()->write((string) json_encode($addCardList));
-
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+            // Invoke the Domain with inputs and retain the result
+            $addCardList = R::store($newCart);
+            for ($i = 0; $i < count($products); $i++) {
+                $beans[] = R::dispense('product_cart');
+                $beans[$i]->product_id = $products[$i]['id'];
+                $beans[$i]->quantity = $products[$i]['quantity'];
+                $beans[$i]->cartId = $addCardList;
+            }
+            $addProductCart = R::storeAll($beans);
+            // Build the HTTP response
+            if (!empty($addCardList) && !empty($addProductCart)) {
+                $response->getBody()->write((string) json_encode(array("message" => "Carrito creado correctamente.")));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+            } else { $response->getBody()->write((string) json_encode(array("message" => "No se ha podido introducir carrito, pruebelo más tarde.")));
+                return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+            }
+        } else {
+            $response->getBody()->write((string) json_encode(array("message" => "Token no valido")));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(500);
+        }
     }
 }
